6a91bb54a4bba63b29c32509b1747584eca08aed
moueza <mouezapeter@gmail.com>
2019-07-12 23:13:59 +0200

    start

---
+++ index.js
@@ -0,0 +1,20 @@
+(
+        function () {
+document.getElementById("xCursor").innerHTML = "Paragraph changed!";
+
+            function showCoords(event) {
+                var x = event.clientX;
+                var y = event.clientY;
+                var coor = "X coords: " + x + ", Y coords: " + y;
+                document.getElementById("demo").innerHTML = coor;
+            }
+
+            function clearCoor() {
+                document.getElementById("demo").innerHTML = "";
+            }
+
+
+
+        }
+)();
+
